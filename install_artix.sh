07e20513ddfd653b2f711768a413aae33e2bf477
Shinyzenith <aakashsensharma@gmail.com>
2021-11-10 23:12:03 +0530

    wi

--- install_artix.sh
+++ install_artix.sh
@@ -1,1 +1,7 @@
-TODO
+#!/usr/bin/env bash
+
+# pacman configs
+sudo sed -i "s/^.*#Color/Color\nILoveCandy/g" /etc/pacman.conf
+sudo sed -i "s/^.*#\(ParallelDownloads\).*$/\1 = 16/g" /etc/pacman.conf
+
+# work in progress
