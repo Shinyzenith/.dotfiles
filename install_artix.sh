ef19ececfac0067025d700e7f27471145f1c775f
Shinyzenith <aakashsensharma@gmail.com>
2021-11-13 07:42:07 +0530

    removed useless scripts

--- install_artix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# pacman configs
-sudo sed -i "s/^.*#Color/Color\nILoveCandy/g" /etc/pacman.conf
-sudo sed -i "s/^.*#\(ParallelDownloads\).*$/\1 = 16/g" /etc/pacman.conf
-
-# work in progress
