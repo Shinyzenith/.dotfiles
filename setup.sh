0347be63a7a75d14ccb6d1ff1fcecf98d6dbcf7a
Shinyzenith <aakashsensharma@gmail.com>
2021-12-24 02:55:03 +0530

    makepkg optimization

--- setup.sh
+++ setup.sh
@@ -15,6 +15,19 @@ cd ~/.config/.dotfiles
 sudo cp ./assets/pacman.conf /etc/pacman.conf
 sudo pacman -Syy
 
+
+# makepkg.conf optimization
+numberofcores=$(grep -c ^processor /proc/cpuinfo)
+if [ $numberofcores -gt 1 ]
+then
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'$(($numberofcores+1))'"/g' /etc/makepkg.conf;
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z - --threads=0)/g' /etc/makepkg.conf
+        sudo sed -i 's/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q - --threads=0)/g' /etc/makepkg.conf
+        sudo sed -i "s/PKGEXT='.pkg.tar.xz'/PKGEXT='.pkg.tar.zst'/g" /etc/makepkg.conf
+else
+        echo "makepkg.conf not changed."
+fi
+
 for line in $(cat ./assets/packagelist)
 do
 	sudo pacman -S --noconfirm --needed $line
