fc5c58f7e87d4bef05ab6b307caca94662fe064f
Shinyzenith <aakashsensharma@gmail.com>
2021-12-31 14:01:53 +0530

    doas

--- assets/sudo.sh
+++ assets/sudo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# script to run as sudo before running setup.sh
-# setting sudo to work without password.
-sudo sed -i "s/^.*#\(%wheel\sALL=(ALL) NOPASSWD: ALL\)/\1\nDefaults \!tty_tickets/g" /etc/sudoers
+# script to run as root before running setup.sh
+# setting doas to work without password.
+echo "permit nopass :wheel" | tee /etc/doas.conf
