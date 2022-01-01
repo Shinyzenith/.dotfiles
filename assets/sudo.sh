f309ef676d371b51520a5f79a85851be8552c166
Shinyzenith <aakashsensharma@gmail.com>
2022-01-01 15:35:24 +0530

    root

--- assets/sudo.sh
+++ assets/sudo.sh
@@ -3,7 +3,8 @@
 # setting doas to work without password.
 pacman -S opendoas --noconfirm --needed
 pacman -Rdd sudo --noconfirm
-echo "permit nopass :wheel" | tee /etc/doas.conf
+echo "permit nopass :wheel" | tee -a /etc/doas.conf
+echo "permit nopass :root" | tee -a /etc/doas.conf
 chown -c root:root /etc/doas.conf
 chmod -c 0400 /etc/doas.conf
 ln -s $(which doas) /usr/bin/sudo
