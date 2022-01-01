5d8626db78e6aa5419a991ef09ad98b2d1cafc8d
Shinyzenith <aakashsensharma@gmail.com>
2022-01-01 15:18:17 +0530

    doas

--- assets/sudo.sh
+++ assets/sudo.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # script to run as root before running setup.sh
 # setting doas to work without password.
+pacman -S opendoas --noconfirm --needed
+pacman -Rdd sudo --noconfirm
 echo "permit nopass :wheel" | tee /etc/doas.conf
 chown -c root:root /etc/doas.conf
 chmod -c 0400 /etc/doas.conf
