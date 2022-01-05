e4e1494561b444da772dda9859ca6ba5aaecbca9
Shinyzenith <aakashsensharma@gmail.com>
2022-01-05 19:13:13 +0530

    remove touchpad.sh

--- .config/river/river/process.sh
+++ .config/river/river/process.sh
@@ -25,6 +25,7 @@ for pad in $(riverctl list-inputs | grep -i touchpad )
 do
   riverctl input $pad events enabled
   riverctl input $pad tap enabled
+  # riverctl input $pad natural-scroll enabled
 done
 light -S $(light -G)
 killall nm-applet
--- .config/river/river/touchpad.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-for pad in $(riverctl list-inputs | grep -i touchpad )
-do
-  riverctl input $pad events enabled
-  riverctl input $pad tap enabled
-  # riverctl input $pad natural-scroll enabled
-done
